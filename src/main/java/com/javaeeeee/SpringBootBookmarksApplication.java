c3e13783301b4fcadeb7f17722d0b39fe412cf0e
Dmitry Noranovich <javaeeeee@gmail.com>
2016-08-08 19:13:20 -0400

    Initial commit

---
+++ src/main/java/com/javaeeeee/SpringBootBookmarksApplication.java
@@ -0,0 +1,12 @@
+package com.javaeeeee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookmarksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookmarksApplication.class, args);
+	}
+}
